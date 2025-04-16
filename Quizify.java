f1b93f2edfd7d52d4ce46d1b6595a17766379a60
S-David-25 <davidlivi25@gmail.com>
2025-04-16 16:57:29 +0530

    Update Quizify.java

--- Quizify.java
+++ Quizify.java
@@ -23,10 +23,10 @@ public class Quizify {
 					setQuestions(sc);
 					break;
 				case 2:
-					takeQuiz(sc);
+					//takeQuiz(sc);        in process
 					break;
 				case 3:
-					viewScoreHistory();
+					//viewScoreHistory();  in process
 					break;
 				case 4:
 					System.out.println("\nExiting Quizify...");
